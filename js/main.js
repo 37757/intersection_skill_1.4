bd3bf326dcfc40caa924fd16cc23f5ec26a79dae
37757 <37757@ma-web.nl>
2025-06-23 10:47:56 +0200

    klaar

--- js/main.js
+++ js/main.js
@@ -1,16 +1,20 @@
 let observer = new IntersectionObserver(
-    function(entries){
-        if(entries[0].isIntersecting === true){
-            console.log("voor 50% in beeld!");
-            document.getElementsByTagName("main")[0].classList.add("a-popup");
-            observer.disconnect();
+    function (entries) {
+        if (entries[0].isIntersecting === true) {
+            console.log("50% in beeld!");
+
+            let nieuwMain = document.createElement("main");
+            nieuwMain.classList.add("fill", "a-popup");
+
+            document.body.insertBefore(nieuwMain, document.getElementsByClassName("trigger")[0]);
+
+        } else {
+            console.log("minder dan 50%");
         }
-        else{
-            console.log("minder 50%");
-        }
-    }, {
+    },
+    {
         threshold: 0.5,
     }
 );
 
-observer.observe(document.getElementsByTagName("main")[0]);
+observer.observe(document.getElementsByClassName("trigger")[0]);
